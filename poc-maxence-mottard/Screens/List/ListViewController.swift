6b39d07122b768a6c2a809589150a7690712e349
MaxenceMottard <maxence.mottard@worldline.com>
2020-09-29 16:38:37 +0200

    fix data

--- poc-maxence-mottard/Screens/List/ListViewController.swift
+++ poc-maxence-mottard/Screens/List/ListViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 class ListViewController: UITableViewController {
     
     let data: [Movie] = [
-        Movie(title: "Joker", description: "Lorem Ipsum", imageUrl: "https://cdn.radiofrance.fr/s3/cruiser-production/2019/08/f67ac277-eae3-4ae0-96e3-3efb2751b573/600_edkfeikxyaegb5i.jpg"),
-        Movie(title: "Interstellar", description: "Lorem Ipsum", imageUrl: "https://imgr.cineserie.com/2020/02/ob_f276dc_interstellar.png?imgeng=/f_jpg/cmpr_0/w_1121/h_1600/m_cropbox&ver=1")
+        Movie(title: "Joker",
+              description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+              imageUrl: "https://cdn.radiofrance.fr/s3/cruiser-production/2019/08/f67ac277-eae3-4ae0-96e3-3efb2751b573/600_edkfeikxyaegb5i.jpg"),
+        Movie(title: "Interstellar",
+              description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+              imageUrl: "https://imgr.cineserie.com/2020/02/ob_f276dc_interstellar.png?imgeng=/f_jpg/cmpr_0/w_1121/h_1600/m_cropbox&ver=1")
     ]
     
     @IBOutlet var dataTableView: UITableView!
