322d2e86e124ca30edd6a2ea71bcba81d3a9c8c8
MaxenceMottard <maxence.mottard@worldline.com>
2020-09-29 14:23:34 +0200

    create movie UITableViewControler

--- poc-maxence-mottard/Screens/List/ListViewController.swift
+++ poc-maxence-mottard/Screens/List/ListViewController.swift
@@ -8,38 +8,40 @@
 import UIKit
 
 class ListViewController: UITableViewController {
+    
+    let data: [Movie] = [
+        Movie(title: "Joker", description: "Lorem Ipsum", imageUrl: "https://cdn.radiofrance.fr/s3/cruiser-production/2019/08/f67ac277-eae3-4ae0-96e3-3efb2751b573/600_edkfeikxyaegb5i.jpg"),
+        Movie(title: "Interstellar", description: "Lorem Ipsum", imageUrl: "https://imgr.cineserie.com/2020/02/ob_f276dc_interstellar.png?imgeng=/f_jpg/cmpr_0/w_1121/h_1600/m_cropbox&ver=1")
+    ]
+    
+    @IBOutlet var dataTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        dataTableView.delegate = self
+        dataTableView.dataSource = self
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return data.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = "Row \(indexPath.row)"
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
