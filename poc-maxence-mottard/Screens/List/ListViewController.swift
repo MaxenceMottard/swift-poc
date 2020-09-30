8ba60df1440a7e949cfdeb4b768d073ba20dbb27
MaxenceMottard <maxence.mottard@worldline.com>
2020-09-30 10:46:01 +0200

    add data to movies

--- poc-maxence-mottard/Screens/List/ListViewController.swift
+++ poc-maxence-mottard/Screens/List/ListViewController.swift
@@ -12,10 +12,14 @@ class ListViewController: UITableViewController {
     let data: [Movie] = [
         Movie(title: "Joker",
               description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-              imageUrl: "https://cdn.radiofrance.fr/s3/cruiser-production/2019/08/f67ac277-eae3-4ae0-96e3-3efb2751b573/600_edkfeikxyaegb5i.jpg"),
+              imageUrl: "https://cdn.radiofrance.fr/s3/cruiser-production/2019/08/f67ac277-eae3-4ae0-96e3-3efb2751b573/600_edkfeikxyaegb5i.jpg",
+              backgroundImageUrl: "https://www.journaldugeek.com/content/uploads/2019/10/4765874-r-1920-1080-f-jpg-q-x-xxyxxx-640x425.jpg",
+              date: "2019-10-09"),
         Movie(title: "Interstellar",
               description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-              imageUrl: "https://imgr.cineserie.com/2020/02/ob_f276dc_interstellar.png?imgeng=/f_jpg/cmpr_0/w_1121/h_1600/m_cropbox&ver=1")
+              imageUrl: "https://imgr.cineserie.com/2020/02/ob_f276dc_interstellar.png?imgeng=/f_jpg/cmpr_0/w_1121/h_1600/m_cropbox&ver=1",
+              backgroundImageUrl: "https://www.slate.fr/sites/default/files/styles/1060x523/public/paramount.jpg",
+              date: "2014-11-05")
     ]
     
     @IBOutlet var dataTableView: UITableView!
