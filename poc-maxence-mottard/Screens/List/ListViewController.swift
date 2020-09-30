6d745f690d67a800b58a30b428f2dc12dff3c649
MaxenceMottard <maxence.mottard@worldline.com>
2020-09-30 12:04:36 +0200

    fix unwrapping

--- poc-maxence-mottard/Screens/List/ListViewController.swift
+++ poc-maxence-mottard/Screens/List/ListViewController.swift
@@ -23,10 +23,10 @@ class ListViewController: UIViewController {
     ]
     
     @IBOutlet var dataTableView: UITableView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         dataTableView.delegate = self
         dataTableView.dataSource = self
         dataTableView.register(UINib(nibName: ListTableViewCell.reuseIdentifier, bundle: nil), forCellReuseIdentifier: ListTableViewCell.reuseIdentifier)
@@ -37,23 +37,28 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.reuseIdentifier, for: indexPath) as? ListTableViewCell else {
-            fatalError("Could not dequeue cell with identifier \(ListTableViewCell.reuseIdentifier)")
+            fatalError("Could not dequeue cell with identifier : \(ListTableViewCell.reuseIdentifier)")
         }
         
         cell.movie = data[indexPath.row]
-
+        
         return cell
     }
-     
-     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         let movie = data[indexPath.row]
-         
-         let detailViewController = storyboard?.instantiateViewController(identifier: "detailViewController") as! DetailViewController
-         detailViewController.movie = movie
-         
-         navigationController?.pushViewController(detailViewController, animated: true)
-     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let movie = data[indexPath.row]
+        
+        let detailViewController = storyboard?.instantiateViewController(identifier: "detailViewController") as? DetailViewController
+        
+        guard let strongVC = detailViewController else {
+            fatalError("Could not instanciate view controller with identifier : detailViewController")
+        }
+        
+        strongVC.movie = movie
+        
+        navigationController?.pushViewController(strongVC, animated: true)
+    }
 }
